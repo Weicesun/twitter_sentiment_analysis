81fcd32e1aa8443ec98d1866f2716a651cb3e784
Weicesun <kevinsun181@gmail.com>
2016-11-24 21:44:51 -0500

    test hot hashtag consumer

--- sparkmaven/tagcount/src/main/scala/tagcount/twitterhottag.scala
+++ sparkmaven/tagcount/src/main/scala/tagcount/twitterhottag.scala
@@ -1,8 +1,28 @@
 package tagcount
+import java.util.*;
+
 import org.apache.spark._;
 
 object twitterhottag {
   def main(args : Array[String]) {
+    if (args.length < 4) {
+        System.err.println("Usage: KafkaWordCount <zkQuorum><group> <topics> <numThreads>")
+        System.exit(1);
+    }
+    val Array(zkQuorum, group, topics, numThreads) = args
+    val sparkConf = new SparkConf().setAppName("countTag")
+    val scc = new StreamContext(sparkConf, Seconds(2))
+    ssc.checkpoint("checkpoint")
+
+    val topicMap = topics.split(",").map((_, numThreads.toInt)).toMap 
+    val lines = KafkaUtils.createStream(ssc, zkQuorum, group, topicMap).map(_,_2)
+    val words = lines.flatMap(_.split(" "))
+    val wordCounts = words.map(x=>(x, 1L))
+    .reduceByKeyAndWindow(_+_, _-_, Minutes(10), Seconds(2), 2)
+    wordCounts.print()
+    ssc.start()
+    ssc.awaitTermination()
+    /*
     val sc = new SparkContext("local", "word", "usr/local/spark",
         Nil, Map(), Map());
     val input = sc.textFile("input.txt");
@@ -11,7 +31,7 @@ object twitterhottag {
     .reduceByKey(_+_)
     
     count.saveAsTextFile("output")
-    System.out.println("ok")
+    System.out.println("ok")*/
   }
   
 }
