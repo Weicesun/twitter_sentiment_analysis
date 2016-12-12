c11be68bcccf27be502db40796de8dc1e40f6387
Weicesun <kevinsun181@gmail.com>
2016-12-11 23:31:06 -0500

    clean and make the stream stable not set up connection too many time

--- app/twitter_sentiment_analysis/KafkaTwitterProducer.java
+++ app/twitter_sentiment_analysis/KafkaTwitterProducer.java
@@ -42,16 +42,6 @@ public class KafkaTwitterProducer {
          public void onStatus(Status status) {      
             queue.offer(status);
 
-            // System.out.println("@" + status.getUser().getScreenName() + " - " + status.getText());
-            // System.out.println("@" + status.getUser().getScreen-Name());
-
-            /*for(URLEntity urle : status.getURLEntities()) {
-               System.out.println(urle.getDisplayURL());
-            }*/
-
-            /*for(HashtagEntity hashtage : status.getHashtagEntities()) {
-               System.out.println(hashtage.getText());
-            }*/
          }
          
          @Override
@@ -101,24 +91,18 @@ public class KafkaTwitterProducer {
       props.put("value.serializer", 
          "org.apache.kafka.common.serialization.StringSerializer");
       
-      Producer<String, String> producer = new KafkaProducer<String, String>(props);
+      Producer<String, String> producer = 
+    		  new KafkaProducer<String, String>(props);
       int i = 0;
       int j = 0;
       
-     // while(i < 10) {
-      while (true) {
-    	  twitterStream.addListener(listener);
-          twitterStream.filter(query);
-
-          Thread.sleep(5000);
+    while(i < 20) {
+    	  
          Status ret = queue.poll();
          
          if (ret == null) {
             Thread.sleep(100);
-            i++;
-            if (i == 10) {
-            	break;
-            }
+            
          }else {
             for(HashtagEntity hashtage : ret.getHashtagEntities()) {
                System.out.println("Hashtag: " + hashtage.getText());
@@ -130,84 +114,7 @@ public class KafkaTwitterProducer {
       producer.close();
       Thread.sleep(5000);
       twitterStream.shutdown();
-		/*LinkedBlockingQueue<Status> queue = new LinkedBlockingQueue<Status>(1000);
-		if (args.length != 5) {
-			System.out.println("KafkaTwitterProducer  <twitter-consumer-key> <twitter-consumer-secret>"
-					+ " <twitter-access-token>"
-					+ "<twitter-access-token-secret>"
-					+ "<topic-name>");
-            return;
-		}
-		String consumerKey = args[0];
-		String consumerSecret = args[1];
-		String accessToken = args[2];
-		String accessTokenSecret = args[3];
-		String topicName = args[4];
 		
-		ConfigurationBuilder cb = new ConfigurationBuilder();
-		cb.setDebugEnabled(true)
-		.setOAuthConsumerKey(consumerKey)
-		.setOAuthConsumerSecret(consumerSecret)
-		.setOAuthAccessToken(accessToken)
-		.setOAuthAccessTokenSecret(accessTokenSecret);
-		TwitterStream twitterStream = new TwitterStreamFactory(cb.build()).getInstance();
-		StatusListener listener = new StatusListener() {
-			public void onStatus(Status status) {
-				queue.offer(status);
-			}
-			public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
-            
-         	}
-         	public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
-
-         	}
-         	public void onScrubGeo(long userId, long upToStatusId) {
-            
-         	}
-         	public void onStallWarning(StallWarning warning) {
-            
-         	}      
-         	public void onException(Exception ex) {
-            	ex.printStackTrace();
-         	}
-		};
-		twitterStream.addListener(listener);
-		Thread.sleep(1000);
-		Properties props = new Properties();
-		props.put("bootstrap.servers", "localhost:9092");
-		props.put("acks", "all");
-		props.put("retries", 0);
-		props.put("batch.size", 16384);
-		props.put("linger.ms", 1);
-		props.put("buffer.memory", 33554432);
-
-		props.put("key.serializer", 
-         "org.apache.kafka.common.serialization.StringSerializer");
-      	props.put("value.serializer", 
-         "org.apache.kafka.common.serialization.StringSerializer");
-
-      	Producer<String, String> producer = new KafkaProducer<String, String>(props);
-      	int i = 0;
-      	int j = 0;
-      	while (i < 10) {
-      		
-      		Status ret = queue.poll();
-      		if (ret == null) {
-      			System.out.println("debug");
-      			Thread.sleep(1000);
-      			i++;
-      		} else {
-      			for (HashtagEntity hashtag : ret.getHashtagEntities()) {
-      				System.out.println("HashTag:" + hashtag.getText());
-      				producer.send(new ProducerRecord<String, String>(topicName, 
-      					Integer.toString(j++), hashtag.getText()));
-      			}
-      		}
-
-      	}
-      	producer.close();
-      	Thread.sleep(5000);
-      	twitterStream.shutdown();*/
 	}
 
 }
--- sparkmaven/tagcount/src/main/scala/tagcount/twitterhottag.scala
+++ sparkmaven/tagcount/src/main/scala/tagcount/twitterhottag.scala
@@ -8,14 +8,14 @@ import org.apache.spark.streaming.kafka._
 object twitterhottag {
   def main(args : Array[String]) {
     if (args.length < 4) {
-        System.err.println("Usage: KafkaWordCount <zkQuorum> <group> <topics> <numThreads>")
+        System.err.println("Usage: KafkaSparkHashTagCount <zkQuorum> <group> <topics> <numThreads>")
         System.exit(1);
     }
     
    // StreamingExamples.setStreamingLogLevels();
     val Array(zkQuorum, group, topics, numThreads) = args
-    val sparkConf = new SparkConf().setAppName("countTag").setMaster("local[2]")
-    val ssc = new StreamingContext(sparkConf, Seconds(2))
+    val sparkConf = new SparkConf().setAppName("countTag").setMaster("local[2]")//run in local mode
+    val ssc = new StreamingContext(sparkConf, Seconds(2))//batch interval 2 second
     
     ssc.checkpoint("checkpoint")
     val topicMap = topics.split(",").map((_, numThreads.toInt)).toMap 
@@ -23,16 +23,15 @@ object twitterhottag {
     val words = lines.flatMap(_.split(" "))
     val topCounts60 = words.map((_, 1)).reduceByKeyAndWindow(_+_, Seconds(60))
     .map{case(topic, count) => (count, topic)}
-    .transform(_.sortByKey(false))
+    .transform(_.sortByKey(false))//descending sort
     topCounts60.foreachRDD(rdd => {
-        val topList = rdd.take(10)
+        val topList = rdd.take(10)//top 10 hashtag content
         println("\n Popular tag in 60 seconds (%s total)".format(rdd.count()))
         topList.foreach{case (count, tag) => println("%s(%s tweets)".format(tag, count))}
         })
-    //val wordCounts = words.map(x => (x, 1L)).reduceByKeyAndWindow(_ + _, _ - _, Minutes(10), Seconds(2), 2)
-    //wordCounts.print()
-    ssc.start()
-    ssc.awaitTermination()
+    
+    ssc.start()//start computation 
+    ssc.awaitTermination()//wait for the computation to terminate
     
   }
   
